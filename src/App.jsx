6febe376525d4796115b056fe2f854685f7a1cab
Alex Norman <alexboston@me.com>
2024-07-06 12:13:30 -0400

    buttons and progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import './App.css'
-import { Button } from 'react-bootstrap';
+import { Button, ProgressBar } from 'react-bootstrap'
+import { useState } from 'react';
+
 
 function App() {
+
+  const [progr, setProgr] = useState(0);
+
+  const handleClick = (event) => {
+    setProgr(progr + 1);
+    event.target.disabled = true;
+  }
   
   return (
     <>
       <div>
       <h1>Black Forest 2</h1>
-      <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+      <Button onClick={handleClick} variant="danger" size="lg" >Duo</Button>{' '}
+      <Button onClick={handleClick} variant="warning" size="lg" >Java</Button>{' '}
+      <Button onClick={handleClick} variant="secondary" size="lg" >Flight</Button>{' '}
+      <Button onClick={handleClick} variant="primary" size="lg" >Burp</Button>{' '}
+      <Button onClick={handleClick} variant="info" size="lg" >Prog</Button>{' '}
+      <Button onClick={handleClick} variant="success" size="lg" >edX</Button>{' '}
+    
       </div>
+      <br></br>
+      <div><ProgressBar now={progr} max={6} min={0} /></div>
+    
     </>
   )
 }
