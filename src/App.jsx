69241b9c79ee9e1a50cc89bdd290d85df15eb878
Alex Norman <alexboston@me.com>
2024-07-05 21:17:14 -0400

    bootstrap and buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Button } from 'react-bootstrap';
 
 function App() {
   
@@ -6,6 +7,14 @@ function App() {
     <>
       <div>
       <h1>Black Forest 2</h1>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
       </div>
     </>
   )
